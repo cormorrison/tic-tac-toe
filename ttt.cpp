36c65472a47239cd2f9bef4c425f9d9f9f1885bd
cormorrison <85662501+cormorrison@users.noreply.github.com>
2021-06-13 22:31:44 -0700

    Update ttt.cpp

--- ttt.cpp
+++ ttt.cpp
@@ -80,30 +80,6 @@ void player_turn(){
     display_board();
 }
 
-//Function to get the game status e.g. GAME WON, GAME DRAW GAME IN CONTINUE MODE
-
-bool gameover(){
-    //checking the win for Simple Rows and Simple Column
-    for(int i=0; i<3; i++)
-    if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
-    return false;
-
-    //checking the win for both diagonal
-
-    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
-    return false;
-
-    //Checking the game is in continue mode or not
-    for(int i=0; i<3; i++)
-    for(int j=0; j<3; j++)
-    if(board[i][j] != 'X' && board[i][j] != 'O')
-    return true;
-
-    //Checking the if game already draw
-    draw = true;
-    return false;
-}
-
 //Program Main Method
 
 int main()
